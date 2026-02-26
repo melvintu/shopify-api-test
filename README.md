7d245cd62a7a18099e600e7a31d85b4fd5339944
melvintu <melvin@meldano.com>
2026-02-26 23:27:34 +1100

    API working and have testing page

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ Run this into CLI to begin the app.
 ```
 shopify app dev 
 ```
+
+Main pages that are used:
+```
+http://127.0.0.1:8000/docs#/
+```
+
+```
+http://127.0.0.1:8000
+```
+
+```
+http://localhost:3457/graphiql
+```
--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import os
 import uvicorn
 from fastapi import FastAPI
+import shopify
+from routers.list_products import router as product_router
 
 app = FastAPI()
+app.include_router(product_router)
 
 @app.get("/")
-def read_root():
-    return {"Hello": "Shopify developer"}
+def root():
+    return {"message": "Main App is Running"}
 
 if __name__ == "__main__":
-    port = uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", "8000")))
+    # Get the port from environment
+    # or default to 8000
+    current_port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=current_port)
